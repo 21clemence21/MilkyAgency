b7ee1bc7f599e2838ec7940e3aba6ff288bf806a
21clemence21 <depernetclemence@gmail.com>
2019-07-24 18:20:29 +0200

    ajout du responsive sur la partie work et d'une gallerie photo - correction des bugs

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,14 @@
-// Select DOM items 
+// Select DOM items for menu
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItem = document.querySelectorAll('.nav-item');
-
 const box2 = document.querySelector('.box2');
+
+// Dom items for gallerie1
+const currentImg = document.querySelector('#current-img');
+const imgs = document.querySelectorAll('.imgs img');
 
 // Set initial State of box2 
 let showBox2 = false;
@@ -16,6 +19,7 @@ let showMenu = false;
 menuBtn.addEventListener('click', toggleMenu); 
 
 function toggleMenu(){
+  console.log("salut");
     if(!showMenu){
       menuBtn.classList.add('close');
       menu.classList.add('show');
@@ -59,3 +63,26 @@ function toggleBox2(){
 
     }
 }
+
+// get image come from sides for section work 
+
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+
+// Section work image gallerie
+imgs.forEach(img => 
+  img.addEventListener('click', e => (currentImg.src = e.target.src))
+  );
