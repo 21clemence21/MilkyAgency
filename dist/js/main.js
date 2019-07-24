36c094cc07a2f04643e83aecc7fe54114eecfe71
21clemence21 <depernetclemence@gmail.com>
2019-07-24 12:09:37 +0200

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,61 @@
+// Select DOM items 
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItem = document.querySelectorAll('.nav-item');
+
+const box2 = document.querySelector('.box2');
+
+// Set initial State of box2 
+let showBox2 = false;
+
+// Set Initial State Of Menu 
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu); 
+
+function toggleMenu(){
+    if(!showMenu){
+      menuBtn.classList.add('close');
+      menu.classList.add('show');
+      menuNav.classList.add('show');
+      menuBranding.classList.add('show');
+      navItem.forEach(item => item.classList.add('show'));
+
+      // Set Menu State 
+      showMenu = true;
+      
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItem.forEach(item => item.classList.remove('show'));
+  
+           // Set Menu State 
+      showMenu = false;
+
+    }
+}
+
+const navWork = document.querySelector('.current');
+navWork.addEventListener('click', toggleBox2); 
+
+function toggleBox2(){
+    if(!showBox2){
+      
+      box2.classList.add('.show');
+
+      // Set Menu State 
+      showBox2 = true;
+      
+    } else {
+        
+        box2.classList.remove('.show');
+  
+           // Set Menu State 
+      showBox2 = false;
+
+    }
+}
